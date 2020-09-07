a9a7673ef7abb093ca93b88f5c140309aa951958
Thiago Jose Fagundes Saquette <43481302+thigosaquette@users.noreply.github.com>
2020-09-07 19:07:48 -0300

    web in 80%

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -4,7 +4,48 @@ import './styles.css';
 
 function Home() {
     return (
-        <h1> Hello World !</h1>
+        <div id="page-home">
+            <div className="container">
+                <header>
+                    <h1 className="title">Aplicação Teste para stream de vídeo e aúdio</h1>
+                    <img 
+                        src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png" 
+                        alt="Imagem da logo React" 
+                        className="logo"
+                    />
+                </header>
+
+                <body>
+                <div className="menuBox">
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
+                    <div className="screen">
+                
+                    </div>
+            
+                </body>
+
+                <footer>
+                    <div className="chatGroup">
+                        <div className="chatBox">
+
+                        </div>
+
+                        <div className="lineChat"></div>
+
+                        <div className="chatInput">
+                            <input type="text" className="chat"/>
+                        </div>
+                    </div>
+
+                    <div className="myScreen">
+
+                    </div>
+                </footer>
+            </div>
+        </div>
     );
 }
 
